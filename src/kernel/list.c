24ba0021de44999522a09990ca89065e20959e03
wavious-jbasista <jbasista@wavious.com>
2021-05-07 10:10:16 -0700

    Add kernel list and fsm elements
    Fixes
      - None
    
    Features
      - Incorporate FSM into kernel
      - Add necessary functions for list usage

---
+++ src/kernel/list.c
@@ -0,0 +1,27 @@
+#include <kernel/list.h>
+
+/*******************************************************************************
+**                          FUNCTION IMPLEMENTATIONS
+*******************************************************************************/
+BaseType_t vListIsItemValueContainedWithin(List_t *pxList, TickType_t xItemValue)
+{
+    ListItem_t *pxListItem;
+    listFOR_EACH_LIST_ITEM(pxListItem, pxList)
+    {
+        if (listGET_LIST_ITEM_VALUE(pxListItem) == xItemValue)
+        {
+            return pdTRUE;
+        }
+    }
+    return pdFALSE;
+}
+
+void vListSplice(List_t *pxListOwner, List_t *pxListAdd)
+{
+    ListItem_t *pxListItem, *pxListItemNext;
+    listFOR_EACH_LIST_ITEM_SAFE(pxListItem, pxListItemNext, pxListAdd)
+    {
+        uxListRemove(pxListItem);
+        vListInsertEnd(pxListOwner, pxListItem);
+    }
+}
