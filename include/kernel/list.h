919606c35bd168d99eac6f23421dfb8a195e40b5
wavious-jbasista <jbasista@wavious.com>
2021-05-07 10:10:16 -0700

    Add kernel list and fsm elements
    Fixes
      - None
    
    Features
      - Incorporate FSM into kernel
      - Add necessary functions for list usage

---
+++ include/kernel/list.h
@@ -0,0 +1,52 @@
+#ifndef _KERNEL_LIST_H_
+#define _KERNEL_LIST_H_
+
+#include <FreeRTOS.h>
+#include <list.h>
+
+/** @brief  Gets pointer to previous list item */
+#define listGET_PREV( pxListItem )  ( ( pxListItem )->pxPrevious )
+
+/** @brief  Loops through list for each item */
+#define listFOR_EACH_LIST_ITEM( pxListItem, pxList )                           \
+    for (pxListItem = listGET_HEAD_ENTRY(pxList);                              \
+         pxListItem != listGET_END_MARKER(pxList);                             \
+         pxListItem = listGET_NEXT(pxListItem))
+
+/** @brief  Loops through list for each item */
+#define listFOR_EACH_LIST_ITEM_SAFE( pxListItem, pxListItemNext, pxList )      \
+    for (pxListItem = listGET_HEAD_ENTRY(pxList),                              \
+         pxListItemNext = listGET_NEXT(pxListItem);                            \
+         pxListItem != listGET_END_MARKER(pxList);                             \
+         pxListItem = pxListItemNext,                                          \
+         pxListItemNext = listGET_NEXT(pxListItemNext))
+
+/**
+ * @brief   List Item Value Contained Within Function
+ *
+ * @details Checks if pxList contains a item which has a value field
+ *          matching xItemValue
+ *
+ * @param[in]   pxList      pointer to list to check in.
+ * @param[in]   xItemValue  item value to check if any ListItem_t contains.
+ *
+ * @return      returns whether list contains a ListItem_t with specified value.
+ * @retval      pdTRUE if list contains an item with specified value.
+ * @retval      pdFALSE if list does not contain an item with specified value.
+ */
+BaseType_t vListIsItemValueContainedWithin(List_t *pxList, TickType_t xItemValue);
+
+/**
+ * @brief   List Splice Function
+ *
+ * @details Splices two lists together by placing all items contained in
+ *          pxListAdd at the end of pxListOwner.
+ *
+ * @param[in]   pxListOwner     pointer to list to be added to.
+ * @param[in]   pxListAdd       pointer to list to add.
+ *
+ * @return      void.
+ */
+void vListSplice(List_t *pxListOwner, List_t *pxListAdd);
+
+#endif /* _KERNEL_LIST_H_ */
