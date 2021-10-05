b078b16cb440a29be69bf15c3017ca9f2c9cbfd0
wpatty <wpatty@wavious.com>
2021-10-05 13:03:01 -0700

    General Message Fields
    Fixes:
      - Added missing field defintions for general messages
    
    Features:
      - None

--- include/drivers/messenger/messages_general.h
+++ include/drivers/messenger/messages_general.h
@@ -33,4 +33,26 @@ typedef enum messages_general_t {
     MESSAGE_GENERAL_END_OF_MESSAGES,
 } messages_general_t;
 
+#define GENERAL_INTF_READY_RESP__MCU_DEVICE_ID__MSK         (0x0000FFFF)
+#define GENERAL_INTF_READY_RESP__MCU_DEVICE_ID__SHFT        (0x00000000)
+#define GENERAL_INTF_READY_RESP__MCU_DEVICE_REV__MSK        (0xFFFF0000)
+#define GENERAL_INTF_READY_RESP__MCU_DEVICE_REV__SHFT       (0x00000010)
+
+#define GENERAL_FW_VER_RESP__PATCH__MSK                     (0x000000FF)
+#define GENERAL_FW_VER_RESP__PATCH__SHFT                    (0x00000000)
+#define GENERAL_FW_VER_RESP__MINOR__MSK                     (0x0000FF00)
+#define GENERAL_FW_VER_RESP__MINOR__SHFT                    (0x00000008)
+#define GENERAL_FW_VER_RESP__MAJOR__MSK                     (0x00FF0000)
+#define GENERAL_FW_VER_RESP__MAJOR__SHFT                    (0x00000010)
+
+#define GENERAL_HW_VER_RESP__MINOR__MSK                     (0x0000FFFF)
+#define GENERAL_HW_VER_RESP__MINOR__SHFT                    (0x00000000)
+#define GENERAL_HW_VER_RESP__MAJOR__MSK                     (0xFFFF0000)
+#define GENERAL_HW_VER_RESP__MAJOR__SHFT                    (0x00000010)
+
+#define GENERAL_MCU_BOOT_RESP__STATUS__MSK                  (0x000000FF)
+#define GENERAL_MCU_BOOT_RESP__STATUS__SHFT                 (0x00000000)
+#define GENERAL_MCU_BOOT_RESP__CODE__MSK                    (0x00FFFF00)
+#define GENERAL_MCU_BOOT_RESP__CODE__SHFT                   (0x00000008)
+
 #endif /* _MESSAGES_GENERAL_H_ */
